389fd52a67525ade6183e164d51efe417aad92ca
bripage <bpage1@nd.edu>
2020-01-27 13:58:47 -0500

    debugging initial compilation

--- structdef.h
+++ structdef.h
@@ -2,9 +2,14 @@
 // Created by bpage1 on 1/27/20.
 //
 
+#ifndef EMUFIREHOSE_STRUCTS_H
+#define EMUFIREHOSE_STRUCTS_H
+
 // Structure to hold packets in workload list
 struct packet {
     unsigned long address;
     long val;
     long flag;
 };
+
+#endif //EMUFIREHOSE_STRUCTS_H

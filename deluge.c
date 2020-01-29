6393e296f651977fac5a699f6e132bb4db7ea54e
bripage <bpage1@nd.edu>
2020-01-29 16:01:15 -0500

    debugging new changes

--- deluge.c
+++ deluge.c
@@ -46,18 +46,16 @@ void spray(long i, long n){
             hits = ATOMIC_ADDM(&address_hits[j], 1);
             if (hits % 24 == 0) {
                 REMOTE_ADD(&event_count, 1);
-                //payload_sum = ATOMIC_ADDM(&hash_state2[j], 1);
                 payload = ATOMIC_ADDM(&payload_state[j], val);
+                ATOMIC_SWAP(&payload_state[j], 0);
                 if (payload <= 5 && flag == 1){
                     REMOTE_ADD(&true_positive, 1);
                 } else if (payload <= 5 && flag == 0){
                     REMOTE_ADD(&false_positve, 1);
                 } else if (payload > 5 && flag == 1){
                     REMOTE_ADD(&false_negative, 1);
-                    ATOMIC_SWAP(&payload_state[j], 0);
                 } else if (payload > 5 && flag == 0){
                     REMOTE_ADD(&true_negative, 1);
-                    ATOMIC_SWAP(&payload_state[j], 0);
                 }
             }
 
@@ -83,16 +81,18 @@ void spray(long i, long n){
             hits = ATOMIC_ADDM(&address_hits[j], 1);
             if (hits % 24 == 0) {
                 REMOTE_ADD(&event_count, 1);
+                //payload_sum = ATOMIC_ADDM(&hash_state2[j], 1);
                 payload = ATOMIC_ADDM(&payload_state[j], val);
-                ATOMIC_SWAP(&payload_state[j], 0);
-                if (payload <= 5 && flag == 1) {
+                if (payload <= 5 && flag == 1){
                     REMOTE_ADD(&true_positive, 1);
-                } else if (payload <= 5 && flag == 0) {
+                } else if (payload <= 5 && flag == 0){
                     REMOTE_ADD(&false_positve, 1);
-                } else if (payload > 5 && flag == 1) {
+                } else if (payload > 5 && flag == 1){
                     REMOTE_ADD(&false_negative, 1);
-                } else if (payload > 5 && flag == 0) {
+                    ATOMIC_SWAP(&payload_state[j], 0);
+                } else if (payload > 5 && flag == 0){
                     REMOTE_ADD(&true_negative, 1);
+                    ATOMIC_SWAP(&payload_state[j], 0);
                 }
             }
         }

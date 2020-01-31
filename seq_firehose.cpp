24f42d561d76a60d9de48d667abbfc7dcbe89755
Brian Page <bpage1@nd.edu>
2020-01-31 15:07:02 -0500

    crated seqeuntial c++ validator

--- seq_firehose.cpp
+++ seq_firehose.cpp
@@ -178,7 +178,7 @@ int main (int argc, char **argv){
 			address_hits[j]++;
 			state = payload_state[j] + 4294967297; // increment both high 32 and low bits by one.
 			hits = (payload_state[j] & 4294967295) +1;
-			printf("hits = %lld\n", hits);
+			//printf("hits = %lld\n", hits);
 			fflush(stdout);
 			if (hits == 24) {
 				event_count++;
@@ -216,7 +216,7 @@ int main (int argc, char **argv){
 			// empty slot, add or update the state for the given location and key
 			address_hits[j]++;
 			state = payload_state[j] + 4294967297; // increment both high 32 and low bits by one.
-			hits = state & 4294967295;
+			hits = (payload_state[j] & 4294967295) +1;
 			printf("hits = %lld\n", hits);
 			fflush(stdout);
 			if (hits == 24) {

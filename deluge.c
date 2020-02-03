f9e1704fc583646db7176a33696fa0628ad8865c
bripage <bpage1@nd.edu>
2020-02-03 14:14:32 -0500

    debugging

--- deluge.c
+++ deluge.c
@@ -59,6 +59,8 @@ void spray(long i, long n){
                 }
                 state = ATOMIC_ADDM(&payload_state[j], (4294967296*val)+1);
                 hits = state & 4294967295;
+                printf("%ld hits = %ld\n", addr, hits);
+                fflush(stdout);
                 payload = state >> 32;
                 if (hits == hit_threshold) {
                     REMOTE_ADD(&stats[0], 1);

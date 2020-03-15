7b6c3adcb2f44c0c56e694d63f3d42e6b3a0d969
Brian Page <bpage1@nd.edu>
2020-03-15 12:06:53 -0400

    corrected migration issue

--- firehose.c
+++ firehose.c
@@ -17,75 +17,78 @@ noinline long main(int argc, char **argv) {
     parse_args(argc, argv);
     init();
 
-    for (long thread_count = 1; thread_count <= 64; thread_count *= 2) {
-        mw_replicated_init(&threads_per_nodelet, thread_count);
-        printf("\nThreads Per Nodelet = %ld\n", threads_per_nodelet);
-        fflush(stdout);
+	for (long thread_count = 1; thread_count <= 64; thread_count *= 2) {
+		mw_replicated_init(&threads_per_nodelet, thread_count);
+		printf("\nThreads Per Nodelet = %ld\n", threads_per_nodelet);
+		fflush(stdout);
 
-        MIGRATE(&payload_state[0]);
+		MIGRATE(&payload_state[0]);
 
-        start_time = CLOCK();
-        cilk_spawn
-        recursive_spawn(0, nodelets_used);
-        cilk_sync;
-        total_time = CLOCK() - start_time;
-        printf("***Computation Complete***\n");
-        fflush(stdout);
+		start_time = CLOCK();
+		cilk_spawn
+		recursive_spawn(0, nodelets_used);
+		cilk_sync;
+		total_time = CLOCK() - start_time;
+		printf("***Computation Complete***\n");
+		fflush(stdout);
 
 
-        for (i = 0; i < nodelets_used; i++) {
-            cilk_migrate_hint(&payload_state[i]);
-            cilk_spawn compile_hit_counts(i);
-        }
-        cilk_sync;
+		MIGRATE(&payload_state[0]);
+		for (i = 0; i < nodelets_used; i++) {
+			cilk_migrate_hint(&payload_state[i]);
+			cilk_spawn compile_hit_counts(i);
+		}
+		cilk_sync;
 
-        long max_occurance = 0, next_addr, unique_keys = 0, temp, migrations = 0, dead_keys = 0;
-        for (i = 0; i < 6400000; i++) {
-            temp = payload_state[i];
-            next_addr = temp >> 32;
-            if (next_addr > max_occurance) {
-                max_occurance = next_addr;
+		long max_occurance = 0, next_addr, unique_keys = 0, temp, migrations = 0, dead_keys = 0;
+		for (i = 0; i < 6400000; i++) {
+			temp = payload_state[i];
+			next_addr = temp >> 32;
+			if (next_addr > max_occurance) {
+				max_occurance = next_addr;
 
-            }
-            if (payload_state[i] != 0) {
-                unique_keys++;
-            }
-            if (next_addr >= 24) {
-                dead_keys++;
-            }
-        }
+			}
+			if (payload_state[i] != 0) {
+				unique_keys++;
+			}
+			if (next_addr >= 24) {
+				dead_keys++;
+			}
+		}
 
-        cleanup1();
-        cilk_spawn
-        recursive_spawn_count(0, nodelets_used);
-        cilk_sync;
-        printf("***Migration Count Run Complete***\n");
-        fflush(stdout);
+		cleanup1();
 
-        execution_time = (double) total_time / CLOCK_RATE;
-        printf("Datums Received: %ld\n", numDatums*nodelets_used);
-        printf("Unique Keys: %ld\n", unique_keys);
-        printf("Max occurance of any key: %ld\n", max_occurance);
-        printf("Event Count: %ld\n", stats[0]);
-        printf("True Anomalies: %ld\n", stats[1]);
-        printf("False Positives: %ld\n", stats[2]);
-        printf("True Negatives: %ld\n", stats[3]);
-        printf("False Negatives: %ld\n", stats[4]);
-        printf("True Bias Flag Count: %ld\n", stats[5]);
-        printf("Dead Keys: %ld\n", dead_keys);
-        printf("Execution Time = %lf msec.\n", execution_time * 1000);
-        printf("Datums per msec = %lf \n", (numDatums * nodelets_used) / (execution_time * 1000));
-        fflush(stdout);
+		MIGRATE(&payload_state[0]);
+		cilk_spawn
+		recursive_spawn_count(0, nodelets_used);
+		cilk_sync;
+		printf("***Migration Count Run Complete***\n");
+		fflush(stdout);
 
-        printf("\nThread Migrations:\n");
-        for (i = 0; i < nodelets_used; i++) {
-            migrations = thread_migrations[i];
-            printf("%ld: %ld\n", i, migrations);
-        }
-        fflush(stdout);
+		execution_time = (double) total_time / CLOCK_RATE;
+		printf("Datums Received: %ld\n", numDatums * nodelets_used);
+		printf("Unique Keys: %ld\n", unique_keys);
+		printf("Max occurance of any key: %ld\n", max_occurance);
+		printf("Event Count: %ld\n", stats[0]);
+		printf("True Anomalies: %ld\n", stats[1]);
+		printf("False Positives: %ld\n", stats[2]);
+		printf("True Negatives: %ld\n", stats[3]);
+		printf("False Negatives: %ld\n", stats[4]);
+		printf("True Bias Flag Count: %ld\n", stats[5]);
+		printf("Dead Keys: %ld\n", dead_keys);
+		printf("Execution Time = %lf msec.\n", execution_time * 1000);
+		printf("Datums per msec = %lf \n", (numDatums * nodelets_used) / (execution_time * 1000));
+		fflush(stdout);
 
-        cleanup();
-    }
+		printf("\nThread Migrations:\n");
+		for (i = 0; i < nodelets_used; i++) {
+			migrations = thread_migrations[i];
+			printf("%ld: %ld\n", i, migrations);
+		}
+		fflush(stdout);
+
+		cleanup();
+	}
 
     return 0;
 }
